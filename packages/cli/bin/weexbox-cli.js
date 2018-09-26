4b8c2feb9af77d6edc773fe1c388bd404724e72f
Mario <mario.huang@aiyuangong.com>
2018-09-26 17:44:21 +0800

    读取版本号

--- packages/cli/bin/weexbox-cli.js
+++ packages/cli/bin/weexbox-cli.js
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
 const program = require('commander')
+const fs = require('fs-extra')
+const path = require('path')
 
 program
+  .version(fs.readJsonSync(path.join(__dirname, '../package.json')).version, '-v, --version')
   .command('create <projectName>')
   .description('使用 weexbox 创建工程')
   .action((projectName) => {
