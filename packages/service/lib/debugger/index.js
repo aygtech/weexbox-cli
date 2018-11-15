f38a7f5083261b4b10b731ba7dbeeda8520ec9ee
Mario <mario.huang@aiyuangong.com>
2018-11-15 11:29:15 +0800

    新结构

--- packages/service/lib/debugger/index.js
+++
@@ -1,7 +0,0 @@
-const debug = require('@weexbox/debugger')
-
-function execute(target) {
-   debug.execute(target)
-}
-
-exports.execute = execute
--- packages/service/lib/webpack/helper.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-const projectPath = (args) => {
-  return path.join(process.cwd(), args)
-}
-
-module.exports = {
-  projectPath
-}
