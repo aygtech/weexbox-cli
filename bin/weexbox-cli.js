432af285fab77c1170c106059ff24da4dfca6faf
Mario <myeveryheart@qq.com>
2019-03-14 17:46:38 +0800

    es6

--- bin/weexbox-cli.js
+++ bin/weexbox-cli.js
@@ -2,13 +2,14 @@
 const program = require('commander')
 const fs = require('fs-extra')
 const path = require('path')
+const create = require('../built/create')
 
 program
   .version(fs.readJsonSync(path.join(__dirname, '../package.json')).version, '-v, --version')
   .command('create <projectName>')
   .description('使用 weexbox 创建工程')
   .action((projectName) => {
-    require('../built/create').Create(projectName)
+    new create.Create(projectName)
   })
 
 program.parse(process.argv)
--- built/create.js
+++ built/create.js
@@ -1,34 +1,33 @@
 "use strict";
-exports.__esModule = true;
-var path_1 = require("path");
-var fs_extra_1 = require("fs-extra");
-var chalk_1 = require("chalk");
-var validateProjectName = require("validate-npm-package-name");
-var download = require("download-git-repo");
-var Create = (function () {
-    function Create(projectName) {
-        var result = validateProjectName(projectName);
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const fs_extra_1 = require("fs-extra");
+const chalk_1 = require("chalk");
+const validateProjectName = require("validate-npm-package-name");
+const download = require("download-git-repo");
+class Create {
+    constructor(projectName) {
+        const result = validateProjectName(projectName);
         if (!result.validForNewPackages) {
-            console.error(chalk_1["default"].red("Invalid project name: '" + projectName + "'"));
-            var errors = result.errors;
+            console.error(chalk_1.default.red(`Invalid project name: '${projectName}'`));
+            const errors = result.errors;
             if (errors) {
-                errors.forEach(function (err) {
-                    console.error(chalk_1["default"].red(err));
+                errors.forEach(err => {
+                    console.error(chalk_1.default.red(err));
                 });
             }
             process.exit(1);
         }
-        var targetDir = path_1.resolve(process.cwd(), projectName);
+        const targetDir = path_1.resolve(process.cwd(), projectName);
         if (fs_extra_1.existsSync(targetDir)) {
-            console.error(chalk_1["default"].red("Target directory " + chalk_1["default"].cyan(targetDir) + " already exists."));
+            console.error(chalk_1.default.red(`Target directory ${chalk_1.default.cyan(targetDir)} already exists.`));
             process.exit(1);
         }
         console.log('正在从https://github.com/aygtech/weexbox-template下载模板\n如果您的网络不好，可以手动下载');
         download('aygtech/weexbox-template', targetDir, function (err) {
-            console.log(err ? err : projectName + " is created!");
+            console.log(err ? err : `${projectName} is created!`);
         });
     }
-    return Create;
-}());
+}
 exports.Create = Create;
 //# sourceMappingURL=create.js.map
