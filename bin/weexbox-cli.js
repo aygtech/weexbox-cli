822a58e5f70941754a1eefafe9e2cb0f2220fc64
llz <383455526@qq.com>
2019-05-20 18:05:39 +0800

    新增创建页面命令： weexbox page pageName --template 'template_dir'
    pageName 支持多级路径：weexbox page pageDir/page
    --template 表示自定义页面创建模板路径，如果不指定则使用默认模板

--- bin/weexbox-cli.js
+++ bin/weexbox-cli.js
@@ -2,13 +2,22 @@
 const program = require('commander')
 const fs = require('fs-extra')
 const path = require('path')
+const lib = require('../lib/index')
 
 program
   .version(fs.readJsonSync(path.join(__dirname, '../package.json')).version, '-v, --version')
   .command('create <projectName>')
   .description('使用 weexbox 创建工程')
   .action((projectName) => {
-    require('../lib/index')(projectName)
+    lib.create(projectName)
   })
+ 
+program
+  .command('page <pageNmae>')
+  .description('在 src 目录下创建页面，支持多级路径，如：weexbox page class1/page')
+  .option('-t, --template <template>', '自定义模板文件所在目录')
+  .action((pageName, options) => {
+    lib.createPage(pageName, options.template)
+  })  
 
 program.parse(process.argv)
---
+++ template/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
